c01a8bace6b596bcfa31e791a44ad9b2443720e2
harrysoung <harrysoung@yahoo.com>
2017-06-09 16:43:00 -0700

    first commit

---
+++ task2.js
@@ -0,0 +1,76 @@
+activities = {
+	Interview = {
+		Special: "The Portal"
+		Month: "Feb"
+		Day: 23
+		Year: 2017
+		Start: "3:00 P.M."
+		End: "4:30 P.M."
+	}
+
+	Lunch = {
+		Special: Cindy
+		Month: "Feb"
+		Day: 25
+		Year: 2017
+		Start: "12:00 P.M."
+		End: "1:00 P.M." 
+	}
+
+	Dinner = {
+		Special: "John"
+		Month: "Feb"
+		Day: 24 
+		Year: 2017
+		Start: "5:00 P.M."
+		End: "5:30 P.M."
+	}
+
+	Conference = {
+		Month: February
+		Day: 24
+		Year: 2017
+		Start: "11:00 A.M"
+		End: "5:30 P.M."
+	}
+
+	Party = {
+		Special: "The Cove"
+		Month: "Feb"
+		Day: 24
+		Year: 2017
+		Start: "3:00 P.M."
+		End: "4:00 P.M."
+	}
+
+	Breakfast = {
+		Month: "Feb"
+		Day: 23
+		Year: 2017
+		Start: "3:30 P.M."
+		End: "4:00 P.M."
+	}
+
+	Study = {
+		Month: "Feb"
+		Day: 13
+		Year: 2017
+		Start: "2:00 P.M."
+		End: "4:00 P.M."
+	}
+
+	Relax = {
+		Month: "Feb"
+		Day: 15
+		Year: 2017
+		Start: "3:00 P.M."
+		End: "5:00 P.M."
+	}
+
+}
+
+for (var elements in activities) {
+	for (var keys in elements){
+		if (elements[keys])
+	}
+}
